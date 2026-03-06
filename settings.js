23da1eeca4be3f3cf04c456a419de45c67787212
armangzoffc <armangzoffc@gmail.com>
2026-03-07 04:49:28 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const configs = {
     // Supabase Credentials
     supabaseUrl: "https://ducxmxnnyhfvguvrfoyf.supabase.co",
     supabaseKey: "sb_publishable_GWNXM4getHBqnzmZw4Edgg_YDZcUxiy",
-    adminUsername: ["ananta","arman"];
-    adminPassword: ["integrity","arman123"]; // Ganti sesukamu
+    adminUsername: ["ananta","arman"],
+    adminPassword: ["integrity","arman123"], // Ganti sesukamu
 };
 
 if (typeof module !== 'undefined') {
