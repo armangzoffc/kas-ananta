94116b877150868a3aaec881bdaa5f4a03e30ac0
armangzoffc <armangzoffc@gmail.com>
2026-03-06 23:56:00 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,11 @@
+const configs = {
+    // Supabase Credentials
+    supabaseUrl: "https://ducxmxnnyhfvguvrfoyf.supabase.co",
+    supabaseKey: "sb_publishable_GWNXM4getHBqnzmZw4Edgg_YDZcUxiy",
+    adminUsername: "ananta",
+    adminPassword: "integrity" // Ganti sesukamu
+};
+
+if (typeof module !== 'undefined') {
+    module.exports = configs;
+}
